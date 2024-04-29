3ee98a7fdc5e5cd99b3130ed28951d34ed39dc00
jake6724 <jakealdridge6724@gmail.com>
2024-04-29 17:25:25 -0400

    Initial movement

---
+++ scripts/player.gd
@@ -0,0 +1,54 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity # negative values go up 
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	if direction != 0:
+		switch_direction(direction)
+
+	move_and_slide()
+	
+	update_animations(direction)
+
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			ap.play("idle")
+		else:
+			ap.play("run")
+	else:
+		if velocity.y < 0: 
+			ap.play("jump")
+		elif velocity.y > 0:
+			ap.play("fall")
+			
+func switch_direction(direction):
+	print("Switch direction called")
+	sprite.flip_h = (direction == -1)
+	sprite.position.x = direction * 4
+	
