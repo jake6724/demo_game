8cf1f5f0f97044af3055febc72c2be3c5fc88007
jake6724 <jakealdridge6724@gmail.com>
2024-05-01 17:19:16 -0400

    Switch enemy sprite direction, and running animation

--- scripts/EnemyUnarmed.gd
+++ scripts/EnemyUnarmed.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name EnemyUnarmed
 
+@onready var ap = $AnimationPlayer
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -8,5 +10,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if velocity.length() > 0:
+		ap.play("Run")
+		
+	if velocity.x > 0:
+		$Sprite2D.flip_h = false
+	elif velocity.x < 0:
+		$Sprite2D.flip_h = true
 	
 	move_and_slide()
